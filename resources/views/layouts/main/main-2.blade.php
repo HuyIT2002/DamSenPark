55a312c49eefe132fc9fda00259e4dbe539f47ed
truongquanghuy100320 <89687943+truongquanghuy100320@users.noreply.github.com>
2024-07-01 00:06:16 +0700

    30/6/2024

--- resources/views/layouts/main/main-2.blade.php
+++ resources/views/layouts/main/main-2.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Hello main</h1>
+
+<div class="content-container">
+    <div class="item">
+        <div class="text">Trò chơi</div>
+    </div>
+    <div class="item">
+        <div class="text">Vườn thú</div>
+    </div>
+    <div class="item">
+        <div class="text">Cảnh đẹp</div>
+    </div>
+    <div class="item">
+        <div class="text highlight">Sân khấu</div>
+    </div>
+    <div class="item">
+        <div class="text">Giáo dục trải nghiệm</div>
+    </div>
+    <div class="item">
+        <div class="text">Ẩm thực</div>
+    </div>
+</div>
 @endsection
